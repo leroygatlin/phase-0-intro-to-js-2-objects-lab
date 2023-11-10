94804e8d989882799af920c48cdabfd1611669e9
leroygatlin <leroygatlin@gmail.com>
2023-11-10 12:14:48 -0800

    complete intro objects lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway',
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newUpdateEmployeeWithKeyAndValue = {...employee};
+    newUpdateEmployeeWithKeyAndValue[key] = value
+    return newUpdateEmployeeWithKeyAndValue;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key, value){
+    const deleteFromEmployeeByKey = {...delete employee.key}
+    return deleteFromEmployeeByKey
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+   delete employee[key]
+    return employee
+}
